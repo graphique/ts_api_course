c0d6f37405a14d58946af2d0ad349cf77cc581b5
Сергей Симченков <sergejsimcenkov@MacBook-Pro-Sergej-2.local>
2025-07-28 23:02:46 +0400

    update category

--- api/categories/methods.ts
+++ api/categories/methods.ts
@@ -1,15 +1,15 @@
-import { brandsRequest } from '../../api/apiClient';
 import * as dataGenerator from '../../testData/dataGenerator';
-import * as baseTest from '../baseTest';
+import { brandsRequest } from '../../api/apiClient';
 
-test('POST /categories', async()=>{
-      const body = {
+export async function createCategory(token: string) {
+    const body = {
         "name": dataGenerator.categoryUniqueName()
       };
       const response = await brandsRequest
       .post("/categories")
       .send(body)
-      .set('Authorization', `Bearer ${baseTest.token}`);
+      .set('Authorization', `Bearer ${token}`);
       expect(response.statusCode).toBe(200);
       expect(response.body.name).toBe(body.name);
-});
+      return response.body;
+}
---
+++ specs/categories/updateCategory.spec.ts
@@ -0,0 +1,23 @@
+import { brandsRequest } from '../../api/apiClient';
+import * as methods from '../../api/categories/methods';
+import * as baseTest from '../baseTest';
+
+let category;
+
+beforeAll(async()=>{
+    category = await methods.createCategory(baseTest.token);
+});
+
+test('PUT/categories{id}',async()=>{
+    const body = {
+        "name": category.name + ' changed'
+    };
+   const id = category._id;
+   const response = await brandsRequest
+   .put(`/categories/${id}`)
+   .send(body)
+   .set('Authorization', `Bearer ${baseTest.token}`);
+   expect(response.statusCode).toBe(200);
+   expect(response.body.name).toEqual(body.name);
+   expect(response.body.name).not.toEqual(category.name);
+});
