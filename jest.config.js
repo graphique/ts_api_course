6007f7f2ee490b743117fcb9e10f723a46761300
Сергей Симченков <sergejsimcenkov@192.168.0.189>
2025-10-08 14:54:15 +0400

    add allure

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  //estEnvironment: "node",
+  testEnvironment: "allure-jest/node",
   preset: 'ts-jest',
   transform: { "^.+\.tsx?$": ["ts-jest",{}],},
   reporters: [
